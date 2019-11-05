779cde1e2745427d068c4f573d2179e8fd134660
donquixote2u <donquixote2u@gmail.com>
2019-11-05 14:05:42 +1300

    modified:   init.lua 	deleted:    webif.lua

--- init.lua
+++ init.lua
@@ -3,15 +3,13 @@
 --new manifest: screen.lua,connectIP.lua, ide.lua, wificredentials.lua
 -- Constants
 require("wificredentials")
--- telnet console 
---manifest: connectIP.lua, ide.lua
--- todo: convert timers to locally defined (general timers deprecated)
- -- Constants
 -- Some control variables
 wifiTrys     = 0      -- reset counter of trys to connect to wifi
 NUMWIFITRYS  = 20    -- Maximum number of WIFI Testings while waiting for connection
+require("WifiConnect")
+function init_webserver()
+dofile("ide.lua")
+end
 initTimeout=2000       -- // timer in ms
 initTimer=tmr.create()  -- // start timer
-initTimer:alarm(initTimeout,tmr.ALARM_SINGLE,function() checkConnection(init_webserver) end)
-dofile("webif.lua")
- 
+initTimer:alarm(initTimeout,tmr.ALARM_SINGLE,function() checkConnection(init_webserver) end) 
