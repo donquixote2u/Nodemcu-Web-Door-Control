18cc872481cac1f91170e3b46b1d561a5128c994
donquixote2u <donquixote2u@gmail.com>
2019-11-06 15:48:03 +1300

    modified:   init.lua 	new file:   webserver.lua

--- init.lua
+++ init.lua
@@ -8,7 +8,8 @@ wifiTrys     = 0      -- reset counter of trys to connect to wifi
 NUMWIFITRYS  = 20    -- Maximum number of WIFI Testings while waiting for connection
 require("WifiConnect")
 function init_webserver()
-dofile("ide.lua")
+dofile("webserver.lua")
+server()
 end
 initTimeout=2000       -- // timer in ms
 initTimer=tmr.create()  -- // start timer
