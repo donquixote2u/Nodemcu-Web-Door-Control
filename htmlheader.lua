151783ec691dcd2d9704dc30afa20e5930571c4e
donquixote2u <donquixote2u@gmail.com>
2019-11-14 20:05:01 +1300

    modified:   htmlheader.lua 	modified:   webserver.lua

--- htmlheader.lua
+++ htmlheader.lua
@@ -1,18 +1,61 @@
-    conn:send('<!DOCTYPE HTML>\n')
-    conn:send('<html>\n')
-    conn:send('<head><meta http-equiv="content-type" content="text/html; charset=UTF-8">\n')
+    function header()
+    local txt='<!DOCTYPE HTML>\n'
+    txt=txt..'<html>\n'
+    txt=txt..'<head><meta http-equiv="content-type" content="text/html; charset=UTF-8">\n'
     -- Scale the viewport to fit the device.
-    conn:send('<meta name="viewport" content="width=device-width, initial-scale=1">')
-    conn:send('<title>Cat Door Latch Control</title>\n')
+    txt=txt..'<meta name="viewport" content="width=device-width, initial-scale=1">'
+    txt=txt..'<title>Cat Door Latch Control</title>\n'
     -- CSS style definition for submit buttons
-    conn:send('<style>\n')
-    conn:send('button[type="submit"] {\n')
-    conn:send('color:yellow; width:100px; padding:10px;\n')
-    conn:send('font: bold 84% "trebuchet ms",helvetica,sans-serif;\n')
-    conn:send('border:1px solid; border-radius: 12px;\n')
-    -- conn:send('transition-duration: 0.4s;\n')
-    conn:send('}\n')
-    conn:send('button[type="submit"]:hover {\n')
-    conn:send('color: white;\n')
-    conn:send('}')
-    conn:send('</style></head>\n')
+    txt=txt..'<style>\n'
+    txt=txt..'button[type="submit"] {\n'
+    txt=txt..'color:yellow; width:100px; padding:10px;\n'
+    txt=txt..'font: bold 84% "trebuchet ms",helvetica,sans-serif;\n'
+    txt=txt..'border:1px solid; border-radius: 12px;\n'
+    txt=txt..'}\n'
+    txt=txt..'button[type="submit"]:hover {\n'
+    txt=txt..'color: white;\n'
+    txt=txt..'}'
+    txt=txt..'</style></head>\n'
+    return txt
+	end
+	
+	function form()
+	local txt='<body>'
+    txt=txt..'<h1>CAT DOOR STATUS</h1>'
+    txt=txt..'<h3> (press to toggle status)</h3>'
+    -- HTML Form (POST type) and buttons.  buttons trigger toggle from OFF to ON and vice versa
+    txt=txt..'<form action="" method="POST">\n'
+    return txt
+    end
+    
+    function button(button_name,button_state,qrystring)
+    local txt='<button type="submit" name="'.button_name.'" ' )
+    fnd = {string.find(qrystring,button_name.."=")}
+    if #fnd ~= 0 then 
+        button_state = string.sub(qrystring,fnd[2]+1)  -- change button state to received value
+	end
+    if button_state=="ON" then
+		value="OFF"
+       colour="green"
+       state_desc="ENABLED"
+    else
+       value="ON"
+       colour="red"
+       state_desc="DISABLED"
+    end
+    txt=txt+'value="'..value..' style="background-color:'..colour..'" >'..button_name..' '..state_desc..'</button><br><br>\n'      
+    -- print("#O")                     
+    txt=txt..'</button><br><br>\n')
+    return txt  
+	end
+	
+	function debugout()
+	local txt="<p>"
+	if(DEBUG=="ON") then
+       for k, v in pairs(Log) do
+        txt=txt..v.."<br>"
+        end
+        txt=txt.."</p>"
+     end   
+        return txt
+	end
--- webserver.lua
+++ webserver.lua
@@ -1,76 +1,27 @@
 -- ESP8266 webserver
 -- This allows control of the door Entry/Exit latch
 --  assumes valid internet connection active (wifi, ip address)
-local Entryvalue = "ON"
-local Exitvalue = "ON"
+ENTRY = "ON"
+EXIT = "ON"
+SERIAL = "OFF"
+DEBUG = "OFF"
 function server()
+	dofile("htmlheader.lua")	-- get html routines	
     srv=net.createServer(net.TCP)
     srv:listen(80,function(conn)
     conn:on("receive",function(conn,payload)
     -- print(payload)  -- View the received data,
-    --get control data from payload
-    fnd = {string.find(payload,"Entry=")}
-    if #fnd ~= 0 then 
-        Entryvalue = string.sub(payload,fnd[2]+1) 
-        if Entryvalue=="ON" then 
-            Entrycolor="green"
-        else  Entryvalue = "OFF"
-             Entrycolor="red"
-        end
-    end 
-    fnd = {string.find(payload,"Exit=")}
-    if #fnd ~= 0 then 
-        Exitvalue = string.sub(payload,fnd[2]+1)
-        if Exitvalue=="ON" then 
-            Exitcolor="green"
-        else  Exitvalue = "OFF"
-             Exitcolor="red"
-        end
-    end 
-    conn:send('<!DOCTYPE HTML>\n')
-    conn:send('<html>\n')
-    conn:send('<head><meta http-equiv="content-type" content="text/html; charset=UTF-8">\n')
-    -- Scale the viewport to fit the device.
-    conn:send('<meta name="viewport" content="width=device-width, initial-scale=1">')
-    conn:send('<title>Cat Door Latch Control</title>\n')
-    -- CSS style definition for submit buttons
-    conn:send('<style>\n')
-    conn:send('button[type="submit"] {\n')
-    conn:send('color:yellow; width:100px; padding:10px;\n')
-    conn:send('font: bold 84% "trebuchet ms",helvetica,sans-serif;\n')
-    conn:send('border:1px solid; border-radius: 12px;\n')
-    conn:send('}\n')
-    conn:send('button[type="submit"]:hover {\n')
-    conn:send('color: white;\n')
-    conn:send('}')
-    conn:send('</style></head>\n')
-    -- HTML body Page content.
-    conn:send('<body>')
-    conn:send('<h1>CAT DOOR STATUS</h1>')
-        conn:send('<h3> (press to toggle status)</h3>')
-   -- HTML Form (POST type) and buttons.  buttons trigger toggle from OFF to ON and vice versa
-    conn:send('<form action="" method="POST">\n')
-    conn:send('<button type="submit" name="Entry" ' )
-    if Entryvalue=="ON" then
-            conn:send('value="OFF" style="background-color:green" >ENTRY ENABLED</button><br><br>\n')
-            print("#B")                      -- entry allowed
-    else
-            conn:send('value="ON" style="background-color:red" >ENTRY DISABLED</button><br><br>\n')
-            print("#O")                      -- exit only
-    end 
-    conn:send('<button type="submit" name="Exit" ')        
-    if Exitvalue=="ON" then
-            conn:send('value="OFF" style="background-color:green" >EXIT ENABLED</button><br><br>\n')
-    else
-            conn:send('value="ON" style="background-color:red" >EXIT DISABLED</button><br><br>\n')
-    end 
-        buf="<p>"
-        for k, v in pairs(Log) do
-        buf=buf..v.."<br>"
-        end
-        buf=buf.."</p>"
-        conn:send(buf);
-    conn:send('</body></html>\n')
+    html=header()				-- output html header/stylesheet
+    conn:send(html)
+	html=form()					-- html body + form header.
+	html=html..button(ENTRY,"ENTRY",payload)
+	html=html..button(EXIT,"EXIT",payload)
+	html=html..button(SERIAL,"SERIAL",payload)
+	html=html..button(DEBUG,"DEBUG",payload)
+	conn:send(html)
+	html=debugout()
+	html=html+'</body></html>\n'
+    conn:send(html)
     conn:on("sent",function(conn) conn:close() end)
     end)
 end)
